97809f43aadbe1591dd3808132dd2109955dcff1
Nicekita <Nicekita100@gmail.com>
2024-07-29 16:05:43 +0700

    docs: added comment to processorPicker

--- src/Payment/ProcessorPicker.php
+++ src/Payment/ProcessorPicker.php
@@ -4,6 +4,7 @@ namespace App\Payment;
 
 use App\Payment\Processors\Paypal;
 use App\Payment\Processors\Stripe;
+use Exception;
 
 class ProcessorPicker
 {
@@ -12,12 +13,16 @@ class ProcessorPicker
     {
     }
 
+    /**
+     * Эту ошибку не надо ловить, т.к. валидация не должна пустить сюда некорректный процессор
+     * @throws Exception
+     */
     public function pickProcessor(string $processor): PaymentProcessor
     {
         return match($processor) {
             'paypal' => $this->paypal,
             'stripe' => $this->stripe,
-            default => throw new \Exception('This payment method is not implemented yet.')
+            default => throw new Exception('This payment method is not implemented yet.')
         };
     }
 
